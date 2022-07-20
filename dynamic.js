3085ae199023470b9b2d2f4a0a2c58a77023d929
Nooruddin Saheb <44988545+nrnsaheb@users.noreply.github.com>
2022-07-20 12:10:43 +0530

    Add files via upload

--- dynamic.js
+++ dynamic.js
@@ -1,5 +1,3 @@
-var apiKey = '10843e07344512f50a28c66bda197ed9';
-
 function fetchProducts() {
 	fetch(`https://dummyjson.com/products`)
 		.then((response) => response.json())
@@ -7,32 +5,133 @@ function fetchProducts() {
 			var containers = document.getElementById('container');
 			console.log(datas);
 			let vav = datas.products;
-			console.log(vav.length);
 			for (let i = 0; i < vav.length; i++) {
+				// console.log(datas.products[i]);
 				let cardContainer = document.createElement('div');
 				let cardTitle = document.createElement('div');
 				let cardBody = document.createElement('div');
 				let image = document.createElement('img');
-				let btn = document.createElement('a');
 				let column = document.createElement('div');
-				column.classList.add('col-md-3', 'my-4');
-				cardContainer.classList.add('card', 'mx-3', 'my-3', 'h-100');
+				column.classList.add('col-md-4', 'my-4');
+				cardContainer.classList.add('card', 'mx-2', 'my-2', 'h-100');
 				cardTitle.classList.add('card-title', 'text-center', 'my-2');
 				image.classList.add('card-image-top', 'mx-auto');
 				image.style.width = '200px';
-				btn.classList.add('btn', 'btn-primary', 'mx-auto', 'my-3');
 				image.style.src = '	200px';
 				cardBody.classList.add('card-body', 'text-center');
 				cardTitle.innerHTML = datas.products[i].title;
 				cardBody.innerHTML = datas.products[i].description;
-				btn.innerHTML = '$' + datas.products[i].price;
-				image.src = datas.products[i].images[0];
+				image.src = datas.products[i].thumbnail;
 				containers.appendChild(column);
 				column.appendChild(cardContainer);
 				cardContainer.appendChild(cardTitle);
 				cardContainer.appendChild(image);
 				cardContainer.appendChild(cardBody);
-				cardContainer.appendChild(btn);
+
+				// Button group?
+				let span = document.createElement('span');
+				span.classList.add('top-0', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger');
+				span.innerHTML = +datas.products[i].stock;
+				let btnGrp = document.createElement('div');
+				btnGrp.classList.add('"btn-group', 'mx-auto');
+				let rating = document.createElement('button');
+				rating.classList.add('btn', 'btn-success', 'mx-3', 'my-3');
+				rating.innerHTML = '⭐' + datas.products[i].rating;
+				let stock = document.createElement('button');
+				stock.classList.add('btn', 'btn-secondary', 'mx-3', 'my-3');
+				stock.innerHTML = 'Stock';
+				let price = document.createElement('button');
+				price.classList.add('btn', 'btn-info', 'mx-3', 'my-3');
+				price.innerHTML = '$' + datas.products[i].price;
+				stock.appendChild(span);
+				btnGrp.appendChild(rating);
+				btnGrp.appendChild(stock);
+				btnGrp.appendChild(price);
+				cardContainer.appendChild(btnGrp);
+			}
+			fetchProductsTwo();
+		});
+}
+
+function fetchProductsTwo() {
+	fetch(`https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/products.json`)
+		.then((response) => response.json())
+		.then((datasTwo) => {
+			var containers = document.getElementById('container');
+			// console.log(datasTwo);
+			// console.log(datasTwo.length);
+			// -2 to even data loading on the page
+			for (let i = 0; i < datasTwo.length - 2; i++) {
+				// console.log(i);
+				// console.log(datasTwo[i].description);
+				let cardContainer = document.createElement('div');
+				let cardTitle = document.createElement('div');
+				let cardBody = document.createElement('div');
+				let image = document.createElement('img');
+				let column = document.createElement('div');
+				column.classList.add('col-md-4', 'my-4');
+				cardContainer.classList.add('card', 'mx-3', 'my-3', 'h-100');
+				cardTitle.classList.add('card-title', 'text-center', 'my-2');
+				image.classList.add('card-image-top', 'mx-auto');
+				image.style.width = '200px';
+				image.style.src = '	200px';
+				cardBody.classList.add('card-body', 'text-center');
+				cardTitle.innerHTML = datasTwo[i].title;
+				// console.log(datasTwo[i].title);
+				cardBody.innerHTML = datasTwo[i].description;
+				image.src = `https://raw.githubusercontent.com/wedeploy-examples/supermarket-web-example/master/ui/assets/images/${i}.jpg`;
+				containers.appendChild(column);
+				column.appendChild(cardContainer);
+				cardContainer.appendChild(cardTitle);
+				cardContainer.appendChild(image);
+				cardContainer.appendChild(cardBody);
+
+				// Button group?
+				let span = document.createElement('span');
+				span.classList.add('top-0', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger');
+				span.innerHTML = datasTwo[i].width;
+				let btnGrp = document.createElement('div');
+				btnGrp.classList.add('"btn-group', 'mx-auto');
+				let rating = document.createElement('button');
+				rating.classList.add('btn', 'btn-success', 'mx-3', 'my-3');
+				rating.innerHTML = '⭐' + datasTwo[i].rating;
+				let stock = document.createElement('button');
+				stock.classList.add('btn', 'btn-secondary', 'mx-3', 'my-3');
+				stock.innerHTML = 'Stock';
+				let price = document.createElement('button');
+				price.classList.add('btn', 'btn-info', 'mx-3', 'my-3');
+				price.innerHTML = '$' + datasTwo[i].price;
+				stock.appendChild(span);
+				btnGrp.appendChild(rating);
+				btnGrp.appendChild(stock);
+				btnGrp.appendChild(price);
+				cardContainer.appendChild(btnGrp);
 			}
 		});
 }
+
+// var containers = document.getElementById('container');
+// function paginated_fetch(
+// 	url = `https://api.punkapi.com/v2/beers?`, // Improvised required argument in JS
+// 	page = 1,
+// 	previousResponse = []
+// ) {
+// 	return fetch(`${url}`) // Append the page number to the base URL
+// 		.then((response) => response.json())
+// 		.then((newResponse) => {
+// 			// let span = document.createElement('span');
+// 			// containers.appendChild(span);
+// 			// span.innerHTML = newResponse[0].name;
+// 			const response = [...previousResponse, ...newResponse]; // Combine the two arrays
+
+// 			// document.write = response[0].name;
+// 			if (newResponse.length !== 0) {
+// 				page++;
+// 				return paginated_fetch(url, page, response);
+// 			}
+
+// 			console.log(newResponse);
+// 			return response;
+// 		});
+// }
+// paginated_fetch(`https://api.punkapi.com/v2/beers?`);
